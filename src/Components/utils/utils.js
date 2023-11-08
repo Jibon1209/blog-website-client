e009dcf78e2d12d03d90301cb6ba7a31ac587f5b
Jibon1209 <jessore007@gmail.com>
2023-11-09 00:55:46 +0600

    added readme live link

--- src/Components/utils/utils.js
+++ src/Components/utils/utils.js
@@ -1,2 +1,4 @@
 //export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://blog-website-server-production.up.railway.app";
+// export const BASE_URL = "https://blog-website-server-production.up.railway.app";
+export const BASE_URL =
+  "https://blog-website-server-production-bf3c.up.railway.app";
